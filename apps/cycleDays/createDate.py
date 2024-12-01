76e248656f8c3c3151f0ca710ef947c3ecb69265
gmalbert <gmalbert+github@gmail.com>
2024-11-30 20:35:35 -0500

    Add files via upload

--- apps/cycleDays/createDate.py
+++ apps/cycleDays/createDate.py
@@ -246,6 +246,7 @@ class CycleDays(hass.Hass):
 		entity = self.args["system_message"]
 		self.set_state(entity, state = "" )
 		
+		# since school years go across calendar years, the holidays will be pulled for the year of the "start date" and then the following year
 		start_date = self.get_state(self.args["start_date"])
 		start_year = datetime.strptime(start_date, '%Y-%m-%d')
 		
@@ -256,11 +257,14 @@ class CycleDays(hass.Hass):
 		entity = self.args["cycle_day_holidays"]
 		status = self.set_state(entity, state = start_year)
 		
+		# Set up the dictionary using the instructions from https://pypi.org/project/holidays/
+		
 		us_holidays = holidays.US(state='NH', years={start_year,next_year})
 		
 		holiday_dates = []
 		holiday_names = []
 		
+		# run through all of the holidays and append to attributes in HA
 		for date, name in us_holidays.items():
 
 			holiday_dates.append(datetime.strftime(date, '%m/%d/%Y'))
@@ -269,7 +273,7 @@ class CycleDays(hass.Hass):
 	
 		holiday_names = list(set(holiday_names))
 		
-		
+		# set the attributes for the names of holidays ("Holidays") and the associated dates ("Holiday Dates")
 		self.set_state(entity, attributes =  {"Holidays" :  holiday_names}  )
 		self.set_state(entity, attributes =  {"Holiday Dates" :  holiday_dates}  )
 		
@@ -284,69 +288,83 @@ class CycleDays(hass.Hass):
 		print("All holidays have been deleted.")
 		
 	def clearNonSchooldays(self, start_date, end_date, old, new, kwargs):
-		
-		entity = "input_text.system_message"
+
+		# to get rid of all manually entered non-school days
+		entity = self.args["system_message"]
 		self.set_state(entity, state = "" )
+		
+		# clear the non-school days list and then update its attributes in HA
+		
 		non_school_days = [self.get_state(self.args["non_school_days"])]
 		non_school_days.clear()
 		self.set_state((self.args["non_school_days"]), attributes =  {"No school days" :  ""}  )
 		print("Non School Days have been deleted.")
-		#entity = "input_text.system_message"
+
+		# Update the input_select to set a single option of "None" as HA does not allow input_select entities without any options.
 		self.call_service("input_select/set_options", entity_id = "input_select.non_school_days", options = "None")
 		self.set_state(self.args["system_message"], state = "<ha-alert alert-type='success'>Non School Days have been deleted.</ha-alert>" )
 
 	def listDates (self, start_date, end_date, old, new, kwargs):
 
-		#entity = "input_text.cycle_day_holidays"
-		holiday_dates = self.get_state("input_text.cycle_day_holidays", attribute="Holiday Dates")
-		print(holiday_dates)
-		print(type(holiday_dates))
-		#return
-		entity = "input_text.system_message"
+
+		holiday_dates = self.get_state(self.args["cycle_day_holidays"], attribute="Holiday Dates")
+
+		entity = self.args["system_message"]
 		self.set_state(entity, state = "" )
 		
+		# Receive the cycle day to start from
 		day_number = self.get_state(self.args["day_number"])
 		day_number = int(float(day_number))
 		
 		non_school_days = [self.get_state(self.args["non_school_days"], attribute="No school days")]
 		
-		print(non_school_days)
-		
+		# add the holidays to the manually created non-school days
 		non_school_days.append(holiday_dates)
 		
+		# get the two date inputs for the start and end date
 		start_date = self.get_state(self.args["start_date"])
 		end_date =  self.get_state(self.args["end_date"])
 
+		# get the individual values for the cycle days, and make it into a list
 		cycle_days = [ self.get_state(self.args["cycle_day_1"]), self.get_state(self.args["cycle_day_2"]), self.get_state(self.args["cycle_day_3"]), self.get_state(self.args["cycle_day_4"]), self.get_state(self.args["cycle_day_5"]) ]
 		
 		delta = timedelta(days=1)
 		
+		# Get the formatted start and end dates
 		start_date = datetime.strptime(start_date, '%Y-%m-%d')
 		end_date = datetime.strptime(end_date, '%Y-%m-%d')
 		
+		# HA creates odd strings where it adds [, ]  "" '
+		# this removes all of those extra characters to just make it a comma deliminted list
 		characters_to_remove = ["[", "]", "!", "'"]
 		
 		for character in characters_to_remove:#
 			non_school_days = str(non_school_days).replace(character, '')
 		
+		# make it back into a list
 		non_school_days = non_school_days.split(", ")
 		
 		non_school_days = set(non_school_days)
 
 		non_school_days = list(non_school_days)
 
-		#print(headers)
-		#return
+		# As long as the initial start date is less than or equal to the provided end date, run this loop
 		while start_date <= end_date:
 			
+			# only check against the non_school_days if the date is a weekday.
+			# check if the current date in the loop is also in the non-school days.
+			# remember that holidays were added to the non-school days earlier in this function.
 			
 			if date.weekday(start_date) < 5 and start_date.strftime("%m/%d/%Y") not in non_school_days:
 
+				# HA requires all day events to start one day and end the next day
 				next_day = start_date + timedelta(days=1)
 				next_day = next_day.strftime('%Y-%m-%d')
 				
+				# set up the request to include the current date in the loop, the next day, the cycle day number, and the special
 				data = {'entity_id': self.args["calendar_name"], 'start_date': start_date.strftime('%Y-%m-%d'), 'end_date': next_day, 'summary': 'Day ' + str(day_number), 'description': cycle_days[day_number-1]}
 				
+				# Run this through the HA REST API
 				response = requests.post(f'{url}', headers=headers, json=data)
 				print(start_date.strftime("%m/%d/%Y") + ' - Day ' + str(day_number) + ' (' + cycle_days[day_number-1] + ') created')
 				#entity = "input_text.system_message"
